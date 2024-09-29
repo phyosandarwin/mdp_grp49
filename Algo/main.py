6091434bfb784e0763f57dab222365dc313675e8
Cragios <35280265+Cragios@users.noreply.github.com>
2024-09-30 00:04:11 +0800

    added json of coords of commands

--- Algo/main.py
+++ Algo/main.py
@@ -1,5 +1,5 @@
 import pygame
-import grid 
+import grid
 import constants
 import jsonParse
 import obstacle
@@ -41,20 +41,24 @@ pygame.init()
 # Set up fonts
 font = pygame.font.Font(None, 36)
 running = True
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), pygame.RESIZABLE)
+screen = pygame.display.set_mode(
+    (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), pygame.RESIZABLE
+)
 pygame.display.set_caption("Simulation")
 
 # Create a Grid object
-obstacles = [obstacle.Obstacle(screen,Position(50,50, Direction.TOP),1), 
-obstacle.Obstacle(screen,Position(90,90, Direction.BOTTOM),2),
-obstacle.Obstacle(screen,Position(40,180, Direction.BOTTOM),3),
-obstacle.Obstacle(screen,Position(120,150, Direction.RIGHT),4),
-obstacle.Obstacle(screen,Position(150,40, Direction.LEFT),5), 
-obstacle.Obstacle(screen,Position(190,190, Direction.LEFT),6) ]
+obstacles = [
+    obstacle.Obstacle(screen, Position(50, 50, Direction.TOP), 1),
+    obstacle.Obstacle(screen, Position(90, 90, Direction.BOTTOM), 2),
+    obstacle.Obstacle(screen, Position(40, 180, Direction.BOTTOM), 3),
+    obstacle.Obstacle(screen, Position(120, 150, Direction.RIGHT), 4),
+    obstacle.Obstacle(screen, Position(150, 40, Direction.LEFT), 5),
+    obstacle.Obstacle(screen, Position(190, 190, Direction.LEFT), 6),
+]
 
 # # Create a Grid object
 # obstacles = [
-#     obstacle.Obstacle(screen,Position(90,90, Direction.BOTTOM),1), 
+#     obstacle.Obstacle(screen,Position(90,90, Direction.BOTTOM),1),
 #     obstacle.Obstacle(screen,Position(90,90, Direction.RIGHT),2),
 #     obstacle.Obstacle(screen,Position(90,90, Direction.TOP),3),
 #     obstacle.Obstacle(screen,Position(90,90, Direction.LEFT),4)
@@ -99,47 +103,145 @@ obstacle.Obstacle(screen,Position(190,190, Direction.LEFT),6) ]
 # result = jsonParse.parse_json(obs)
 # obstacles = jsonParse.convert_json(screen, result)
 
-#buttons
+# buttons
 button_list = constants.BUTTON_LIST
 
 
-grid = grid.Grid(screen,obstacles)
-robot = robot.Robot(screen,grid, 0, 0)
+grid = grid.Grid(screen, obstacles)
+robot = robot.Robot(screen, grid, 0, 0)
 print()
 # hamiltonian = hamiltonian.Hamiltonian(robot,grid)
 robot.setCurrentPos(0, 0, Direction.TOP)
 
 
 # hamiltonian.get_path()
-robot.setCurrentPos(constants.ROBOT_SAFETY_DISTANCE, constants.ROBOT_SAFETY_DISTANCE, Direction.TOP)
-hamiltonian = hamiltonian.Hamiltonian(robot,grid)
+robot.setCurrentPos(
+    constants.ROBOT_SAFETY_DISTANCE, constants.ROBOT_SAFETY_DISTANCE, Direction.TOP
+)
+hamiltonian = hamiltonian.Hamiltonian(robot, grid)
 hamiltonian.get_path()
 
+
 def get_commands(commands):
-  output = []
-  
-  for command in commands:
-    output.append(command.rpi_message())
-    
-  return ','.join(output)
+    output = []
+
+    for command in commands:
+        output.append(command.rpi_message())
+
+    return ",".join(output)
+
 
 commands_str = get_commands(hamiltonian.commands)
 
 # "target": "AND"
 rpi_commands = {
-  "target": "STM", 
-  "cat": "path",
-  "value": {
-    "commands": commands_str
-  },
-
+    "target": "STM",
+    "cat": "path",
+    "value": {"commands": commands_str},
 }
+
+
+# adding coords_str
+def add_coords(command, dir):
+    x, y = 0, 0
+    if (command[:2]) == "SF":
+        if dir == 0:
+            y += int(command[2:])
+        if dir == 1:
+            x += int(command[2:])
+        if dir == 2:
+            y -= int(command[2:])
+        if dir == 3:
+            x -= int(command[2:])
+    elif (command[:2]) == "SB":
+        if dir == 0:
+            y -= int(command[2:])
+        if dir == 1:
+            x -= int(command[2:])
+        if dir == 2:
+            y += int(command[2:])
+        if dir == 3:
+            x += int(command[2:])
+    elif (command[:2]) == "LF":
+        if dir == 0:
+            x -= 30
+            y += 20
+        if dir == 1:
+            x += 20
+            y += 30
+        if dir == 2:
+            x += 30
+            y -= 20
+        if dir == 3:
+            x -= 20
+            y -= 30
+        dir -= 1
+        dir %= 4
+    elif (command[:2]) == "RF":
+        if dir == 0:
+            x += 30
+            y += 20
+        if dir == 1:
+            x += 20
+            y -= 30
+        if dir == 2:
+            x -= 30
+            y -= 20
+        if dir == 3:
+            x -= 20
+            y += 30
+        dir += 1
+        dir %= 4
+    elif (command[:2]) == "LB":
+        if dir == 0:
+            x -= 20
+            y -= 30
+        if dir == 1:
+            x -= 30
+            y += 20
+        if dir == 2:
+            x += 20
+            y += 30
+        if dir == 3:
+            x += 30
+            y -= 20
+        dir += 1
+        dir %= 4
+    elif (command[:2]) == "RB":
+        if dir == 0:
+            x += 20
+            y -= 30
+        if dir == 1:
+            x -= 30
+            y -= 20
+        if dir == 2:
+            x -= 20
+            y += 30
+        if dir == 3:
+            x += 30
+            y += 20
+        dir -= 1
+        dir %= 4
+    return (x, y, dir)
+
+
+coords = {"coords": []}
+x, y, dir = 0, 0, 0
+for command in commands_str.split(","):
+    _x, _y, _dir = add_coords(command, dir)
+    x += _x
+    y += _y
+    dir = _dir
+    coords["coords"].append((x, y, dir))
+
+print(coords)
 
 rpi_commands_json = json.dumps(rpi_commands)
 
 print(rpi_commands)
 
 # client_socket.send(rpi_commands_json.encode('utf-8'))
+# client_socket.send(coords_json.encode("utf-8"))
 
 robot.setCurrentPos(0, 0, Direction.TOP)
 
@@ -151,23 +253,35 @@ while running:
             if event.button == 1:  # Left mouse button
                 mouse_x, mouse_y = event.pos
                 handle_button_click(event.pos, robot, button_list)
-                if (constants.START_BUTTON['x'] <= mouse_x <= constants.START_BUTTON['x'] + constants.START_BUTTON['width'] and
-                    constants.START_BUTTON['y'] <= mouse_y <= constants.START_BUTTON['y'] + constants.START_BUTTON['height']):
+                if (
+                    constants.START_BUTTON["x"]
+                    <= mouse_x
+                    <= constants.START_BUTTON["x"] + constants.START_BUTTON["width"]
+                    and constants.START_BUTTON["y"]
+                    <= mouse_y
+                    <= constants.START_BUTTON["y"] + constants.START_BUTTON["height"]
+                ):
                     run_algo(robot, grid)
-                
 
     screen.fill((224, 235, 235))
     for button in button_list:
-        draw_button(screen, button['path'], button['x'], button['y'], button['width'], button['height'], (169,169,169),(128, 128, 128))
+        draw_button(
+            screen,
+            button["path"],
+            button["x"],
+            button["y"],
+            button["width"],
+            button["height"],
+            (169, 169, 169),
+            (128, 128, 128),
+        )
     draw_text_button(screen, constants.START_BUTTON)
 
     # if count == 0:
-        
-        # robot.draw_robot()
-        # count = run_algo(robot, hamiltonian.commands, grid, count)
+
+    # robot.draw_robot()
+    # count = run_algo(robot, hamiltonian.commands, grid, count)
 
     grid.draw_grid(visitedSquares)
     robot.draw_robot()
     pygame.display.update()
-
-    
